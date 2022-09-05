35a6a12e097f9b615e53447622e7cd9cb4187c47
Kanishk <Kanishk333gupta@gmail.com>
2022-09-05 11:55:06 +0530

    Added folders and files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [result, setResult] = useState("");
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name));
+  };
+  const clear = () => {
+    setResult("");
+  };
+  const calculate = () => {
+    try {
+      setResult(eval(result));
+    } catch (error) {
+      setResult("Error - Wrong input");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="center">
+      <div class="container">
+        <div class="screen">
+          <form>
+            <input type="text" value={result} />
+          </form>
+        </div>
+        <div class="keyPad">
+          <button onClick={clear} class="ac-btn light-gray">
+            AC
+          </button>
+          <button name="%" onClick={handleClick} class="orange">
+            %
+          </button>
+          <button name="/" onClick={handleClick} class="orange">
+            /
+          </button>
+          <button name="7" onClick={handleClick} class="light-gray">
+            7
+          </button>
+          <button name="8" onClick={handleClick} class="light-gray">
+            8
+          </button>
+          <button name="9" onClick={handleClick} class="light-gray">
+            9
+          </button>
+          <button name="*" onClick={handleClick} class="orange">
+            &times;
+          </button>
+          <button name="4" onClick={handleClick} class="light-gray">
+            4
+          </button>
+          <button name="5" onClick={handleClick} class="light-gray">
+            5
+          </button>
+          <button name="6" onClick={handleClick} class="light-gray">
+            6
+          </button>
+          <button name="-" onClick={handleClick} class="orange">
+            &ndash;
+          </button>
+          <button name="3" onClick={handleClick} class="light-gray">
+            3
+          </button>
+          <button name="2" onClick={handleClick} class="light-gray">
+            2
+          </button>
+          <button name="1" onClick={handleClick} class="light-gray">
+            1
+          </button>
+          <button name="+" onClick={handleClick} class="orange">
+            +
+          </button>
+          <button name="0" onClick={handleClick} class="light-gray">
+            0
+          </button>
+          <button name="." onClick={handleClick} class="light-gray">
+            .
+          </button>
+          <button onClick={calculate} class="equal-btn">
+            =
+          </button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
